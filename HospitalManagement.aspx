f70ccec30787e5219da27a9fa38c4e82960cb9a2
PrasenjeetKamble <prasensk1999@gmail.com>
2026-03-02 14:38:57 +0530

    Add files via upload
    Initial Project Upload

---
+++ HospitalManagement.aspx
@@ -0,0 +1,146 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HospitalManagement.aspx.cs" Inherits="HospitalManagement.HospitalManagement" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+    <div>
+
+    
+    <h2> Hospital Appointment Portal   </h2>
+   <br />
+     
+       <script type="text/javascript">
+           function allowOnlyLetters(e) {
+               var charCode = e.which ? e.which : e.keyCode;
+
+               // Allow letters (A-Z, a-z) and space
+               if ((charCode >= 65 && charCode <= 90) ||
+                (charCode >= 97 && charCode <= 122) ||
+                charCode == 32) {
+                   return true;
+               }
+               else {
+                   return false;
+               }
+           }
+    </script>
+
+    Patient Name: <asp:TextBox ID="txtName" runat="server" onkeypress="return allowOnlyLetters(event)"></asp:TextBox> 
+
+    <asp:RequiredFieldValidator 
+    ID="rfvName" 
+    runat="server" 
+    ControlToValidate="txtName"
+    ErrorMessage="Patient Name is required"
+    ForeColor="Red"
+    Display="Dynamic">
+    </asp:RequiredFieldValidator>
+
+  
+        <br />
+        <br />
+
+    Doctor Name: <asp:DropDownList ID="ddlDoctor" runat="server">
+        <asp:ListItem Value ="0">Select</asp:ListItem>
+        <asp:ListItem>Dr. Patil (General)</asp:ListItem>
+        <asp:ListItem>Dr. Deshmukh (Orthopedic)</asp:ListItem>
+        <asp:ListItem>Dr. Sharma (Cardiologist)</asp:ListItem>
+        <asp:ListItem>Dr. Reddy (Dermatologist)</asp:ListItem>
+
+    </asp:DropDownList> 
+
+    <asp:RequiredFieldValidator 
+    ID="rfvDoctor" 
+    runat="server" 
+    ControlToValidate="ddlDoctor"
+    InitialValue="0"
+    ErrorMessage="Please select a doctor"
+    ForeColor="Red"
+    Display="Dynamic">
+    </asp:RequiredFieldValidator>
+        <br />
+        <br />
+
+        
+    Date: <asp:TextBox ID="txtDate" runat="server" TextMode="Date"></asp:TextBox> 
+
+    <asp:RequiredFieldValidator 
+    ID="rfvDate" 
+    runat="server" 
+    ControlToValidate="txtDate"
+    ErrorMessage="Date is required"
+    ForeColor="Red"
+    Display="Dynamic">
+    </asp:RequiredFieldValidator>
+
+    <br /><br />
+
+        <asp:Button ID="Button1" runat="server" Text="ADD" BackColor="#339966" 
+            ForeColor="White" onclick="Button1_Click" ViewStateMode="Enabled" />
+            
+        &nbsp;&nbsp;&nbsp;&nbsp;
+            
+        <asp:Button ID="Button2" runat="server" Text="MODIFY" BackColor="#0000CC" 
+            ForeColor="White" onclick="Button2_Click" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Text="DELETE" BackColor="#CC0000" 
+            ForeColor="White" onclick="Button3_Click" />
+
+
+        <br />
+
+        <asp:HiddenField ID="hfAppID" runat="server" />
+
+
+
+</div>
+
+<hr />
+
+
+
+<%--  <asp:GridView ID="gvAppointments" runat="server" CellPadding="10"
+    AutoGenerateColumns="True" 
+    DataKeyNames="AppID"
+    OnSelectedIndexChanged="gvAppointments_SelectedIndexChanged">
+
+    <Columns>
+        <asp:CommandField ShowSelectButton="True" />
+    </Columns>
+
+</asp:GridView>--%>
+
+<asp:GridView ID="gvAppointments" runat="server" CellPadding="10"
+    AutoGenerateColumns="False" 
+    DataKeyNames="AppID"
+    OnSelectedIndexChanged="gvAppointments_SelectedIndexChanged">
+
+    <Columns>
+        <asp:CommandField ShowSelectButton="True" />
+        
+        <asp:BoundField DataField="AppID" HeaderText="AppID" />
+        <asp:BoundField DataField="PatientName" HeaderText="Patient Name" />
+        <asp:BoundField DataField="DoctorName" HeaderText="Doctor Name" />
+
+     
+        <asp:BoundField DataField="AppDate" HeaderText="AppDate" 
+                        DataFormatString="{0:dd-MMM-yyyy hh:mm tt}" HtmlEncode="false" />
+
+        <asp:BoundField DataField="Status" HeaderText="Status" />
+    </Columns>
+
+</asp:GridView>
+
+
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ HospitalManagement.aspx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace HospitalManagement
+{
+    public partial class HospitalManagement : System.Web.UI.Page
+
+    {
+        string connStr = "Data Source=DESKTOP-5QNS0TK;Initial Catalog=HotelManagement;Integrated Security=True";
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            { 
+                LoadGrid(); 
+            }
+        }
+
+        void LoadGrid()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Appointments", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvAppointments.DataSource = dt;
+            gvAppointments.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            string query = "INSERT INTO Appointments (PatientName, DoctorName, AppDate) VALUES (@name, @doc, @date)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@name", txtName.Text);
+            cmd.Parameters.AddWithValue("@doc", ddlDoctor.SelectedValue);
+            DateTime bookingDateTime = Convert.ToDateTime(txtDate.Text).Add(DateTime.Now.TimeOfDay);
+            cmd.Parameters.AddWithValue("@date", bookingDateTime);
+            //cmd.Parameters.AddWithValue("@date", DateTime.Now);
+           // cmd.Parameters.AddWithValue("@date", Convert.ToDateTime(txtDate.Text));
+            // Current Time ke saath date bhejne ke liye
+            //DateTime bookingDateTime = DateTime.Parse(txtDate.Text).Add(DateTime.Now.TimeOfDay);
+            //cmd.Parameters.AddWithValue("@date", bookingDateTime);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            LoadGrid(); // Grid refresh karega
+
+            Response.Redirect(Request.RawUrl);
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            
+
+            if (hfAppID.Value == "")
+            {
+                return; // koi row select nahi hui
+            }
+
+            int appId = Convert.ToInt32(hfAppID.Value);
+
+            SqlConnection conn = new SqlConnection(connStr);
+            string query = "UPDATE Appointments SET Status = @status WHERE AppID = @id";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@status", "Confirmed");
+            cmd.Parameters.AddWithValue("@id", appId);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            LoadGrid();
+
+         
+         ScriptManager.RegisterStartupScript(this, this.GetType(), "alert",
+        "alert('Appointment Confirmed Successfully!');", true);
+
+         txtName.Text = "";
+         ddlDoctor.SelectedIndex = 0;
+         txtDate.Text = "";
+         hfAppID.Value = "";
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+            if (hfAppID.Value != "")
+            {
+                SqlConnection conn = new SqlConnection(connStr);
+
+                string query = "DELETE FROM Appointments WHERE AppID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", hfAppID.Value);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                LoadGrid();
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alert",
+                    "alert('Appointment Deleted Successfully!');", true);
+
+                // Clear fields after delete
+                txtName.Text = "";
+                ddlDoctor.SelectedIndex = 0;
+                txtDate.Text = "";
+                hfAppID.Value = "";
+            }
+
+
+        }
+
+
+        protected void gvAppointments_SelectedIndexChanged(object sender, EventArgs e)
+        {           
+
+
+            int appId = Convert.ToInt32(gvAppointments.SelectedDataKey.Value);
+            hfAppID.Value = appId.ToString();
+
+            SqlConnection conn = new SqlConnection(connStr);
+            string query = "SELECT * FROM Appointments WHERE AppID = @id";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", appId);
+
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                txtName.Text = dr["PatientName"].ToString();
+                ddlDoctor.SelectedValue = dr["DoctorName"].ToString();
+
+                DateTime dt = Convert.ToDateTime(dr["AppDate"]);
+                txtDate.Text = dt.ToString("yyyy-MM-dd");
+            }
+
+            conn.Close();
+
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
